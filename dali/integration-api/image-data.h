666024dc57ddd4a4c5f54489f1d71e75fe7282a5
Andrew Cox <andrew.cox@partner.samsung.com>
2014-03-06 11:47:03 +0000

    -- DRAFT -- Move Bitmap class out of integration API -- DRAFT --
    [Issue#] N/A
    
    [Problem]
    Bitmap is a complicated abstract base class with several
    interactions across core. Exposing it to the adaptor for the simple
    job of moving some pixels and metadata from disk and application
    memory buffers back to the core is unnecessary complication.
    
    [Cause]
    Legacy implementation.
    
    [Solution]
    Introduce a simpler and concrete ImageData class, then update all users
    of Bitmap to employ this instead.
    
    * Added ImageData resource type and resource type id.
    
    * GetGlyphData changed name of param from bitmap to image data.
    
    * Introduced the ImageData class to core and got it all to build.
    
    * reverted resource-request.h changes as unrelated to rest of patch.
    
    * Plumbed-in resource manager LoadResponce().
    * Pixel::IsEncoded().
    * Bitmap::New(pixel::format).
    
    * Moved new pixel stuff out of CAPI to new core file: pixel-extra.h.
    
    * Gave ImageData the ability to take over and own a buffer given to it on construction.
    
    * Added stdint.h dependency to ImageData's header.
    
    * Tidied-up ImageData and documented it.
    
    Change-Id: I6150b29121adca9c7be84b7b0620d6406d6717ca
    Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

---
+++ dali/integration-api/image-data.h
@@ -0,0 +1,194 @@
+#ifndef __DALI_INTEGRATION_IMAGE_DATA_H__
+#define __DALI_INTEGRATION_IMAGE_DATA_H__
+
+//
+// Copyright (c) 2014 Samsung Electronics Co., Ltd.
+//
+// Licensed under the Flora License, Version 1.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://floralicense.org/license/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// INTERNAL INCLUDES
+#include <dali/integration-api/debug.h> // For DALI_LOG_OBJECT_STRING_DECLARATION
+#include <dali/public-api/object/ref-object.h>
+#include <dali/public-api/common/intrusive-ptr.h>
+#include <dali/public-api/images/pixel.h>
+
+// EXTERNAL INCLUDES
+#include <stdint.h>
+
+namespace Dali
+{
+
+namespace Integration
+{
+
+class ImageData;
+typedef IntrusivePtr<ImageData> ImageDataPtr;
+
+/**
+ * A simple container for image data. Just a pointer to a buffer and some
+ * minimal metadata. It always owns the buffer of image data that it points to
+ * until it is destroyed or the buffer is released with ReleaseImageBuffer().
+ *
+ * The width and height stored are the logical values. So if the pixel format
+ * imposes certain size constraints such as the multiple of 4 rounding-up of
+ * ETC, that should be taken account of by client code.
+ *
+ */
+class DALI_IMPORT_API ImageData : public Dali::RefObject
+{
+  /**
+   * Construct an instance and allocate a buffer owned by it.
+   * @param[in] numBytes The number of bytes to allocate to hold the image data.
+   * @param[in] imageWidth The width in pixels of the image.
+   * @param[in] imageHeight The height in pixels of the image.
+   * @param[in] pixelFormat The format of the image data in the buffer.
+   */
+  ImageData( const size_t numBytes, const unsigned imageWidth, const unsigned imageHeight, const Pixel::Format pixelFormat ) :
+    mData( new uint8_t[numBytes] ),
+    mDataSize( numBytes ),
+    imageWidth( imageWidth ),
+    imageHeight( imageHeight ),
+    pixelFormat( pixelFormat )
+  {
+    DALI_ASSERT_DEBUG( numBytes > 0U && imageWidth > 0U && imageHeight > 0U );
+  }
+
+  /**
+   * Construct an instance and have it take ownership of the buffer passed in.
+   * @param[in] imageBuffer A buffer of pixel data that should have been allocated using <code>new uint8_t[]</code>.
+   * @param[in] numBytes The number of bytes in the buffer pointed to by imageBuffer.
+   * @param[in] imageWidth The width in pixels of the image.
+   * @param[in] imageHeight The height in pixels of the image.
+   * @param[in] pixelFormat The format of the image data in the buffer.
+   */
+  ImageData( uint8_t * const imageBuffer, const size_t numBytes, const unsigned imageWidth, const unsigned imageHeight, const Pixel::Format pixelFormat ) :
+    mData( imageBuffer ),
+    mDataSize( numBytes ),
+    imageWidth( imageWidth ),
+    imageHeight( imageHeight ),
+    pixelFormat( pixelFormat )
+  {
+    DALI_ASSERT_DEBUG( imageBuffer != 0 && numBytes > 0U && imageWidth > 0U && imageHeight > 0U );
+  }
+
+  ~ImageData() { delete[] mData; }
+
+public:
+
+  /** Allocate and initialize a fresh ImageData object pointing at a new buffer,
+   * returning a smart pointer owning it.
+   * @note Clients can alternatively use the helper function NewBitmapImageData() to
+   * calculate the buffer size from other parameters if the image data represents
+   * a bitmap (a 2d grid of addressable pixels).
+   * @param[in] numBytes The number of bytes to allocate to hold the image data.
+   * @param[in] imageWidth The width in pixels of the image.
+   * @param[in] imageHeight The height in pixels of the image.
+   * @param[in] pixelFormat The format of the image data in the buffer.
+   **/
+  static ImageDataPtr New( const size_t numBytes, unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat );
+
+  /** Allocate and initialise a fresh ImageData object, taking ownership of the
+   * buffer passed in.
+   * @param[in] imageBuffer A block of memory allocated with
+   * <code>new uint8_t[]</code>. <em>This is owned by the new ImageData instance
+   * on successful return and should be forgotten by the caller</em>.
+   * @param[in] numBytes The number of bytes in the buffer pointed to by imageBuffer.
+   * @param[in] imageWidth The width in pixels of the image.
+   * @param[in] imageHeight The height in pixels of the image.
+   * @param[in] pixelFormat The format of the image data in the buffer.
+   **/
+  static ImageDataPtr New( uint8_t * const imageBuffer, const size_t numBytes, unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat );
+
+  /** Access the buffer of image data. */
+  const uint8_t * GetBuffer() const { return mData; }
+
+  /** Access the buffer of image data. */
+  uint8_t * GetBuffer() { return mData; }
+
+  /**
+   * You now own this buffer. Act wisely and call delete[] on it one day.
+   **/
+  uint8_t * ReleaseImageBuffer() { uint8_t * const data = mData; mData = 0; return data; }
+
+private:
+
+  ImageData(const ImageData& other);  ///< defined private to prevent use
+  ImageData& operator = (const ImageData& other); ///< defined private to prevent use
+
+  /** Pointer to the buffer of image data. */
+  uint8_t*     mData;
+
+public:
+  const size_t        mDataSize;   ///< Number of bytes in buffer pointed at by mData
+  const uint16_t      imageWidth;  ///< Image logical width in pixels
+  const uint16_t      imageHeight; ///< Image logical height in pixels
+  const Pixel::Format pixelFormat; ///< Pixel format
+
+  // Changes scope, should be at end of class
+  DALI_LOG_OBJECT_STRING_DECLARATION;
+};
+
+inline ImageDataPtr ImageData::New(  const size_t numBytes, unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat )
+{
+  DALI_ASSERT_DEBUG( numBytes > 0 && "Zero allocations are pointless if also harmless."  );
+  DALI_ASSERT_DEBUG( imageWidth > 0 && imageHeight > 0 && "Zero dimensioned images are pointless if also harmless." );
+  return ImageDataPtr( new ImageData( numBytes, imageWidth, imageHeight, pixelFormat ) );
+}
+
+inline ImageDataPtr ImageData::New( uint8_t * const imageBuffer, const size_t numBytes, unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat )
+{
+  DALI_ASSERT_DEBUG( numBytes > 0 && "Zero-length buffers are pointless if also harmless."  );
+  DALI_ASSERT_DEBUG( imageWidth > 0 && imageHeight > 0 && "Zero dimensioned images are pointless if also harmless." );
+  return ImageDataPtr( new ImageData( imageBuffer, numBytes, imageWidth, imageHeight, pixelFormat ) );
+}
+
+/**
+ * A convenience function for creating the common case of an uncompressed image
+ * having exactly width * height pixels in the buffer with no padding bytes at
+ * row ends and no extra rows.
+ * @param[in] imageWidth The width in pixels of the image.
+ * @param[in] imageHeight The height in pixels of the image.
+ * @param[in] pixelFormat The format of the image data in the buffer.
+ */
+inline ImageDataPtr NewBitmapImageData( unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat )
+{
+  DALI_ASSERT_DEBUG( pixelFormat <= Pixel::BGRA8888 && "Pixel format must be an addressable one." );
+  const size_t numBytes = imageWidth * imageHeight * Pixel::GetBytesPerPixel( pixelFormat );
+
+  return ImageData::New( numBytes, imageWidth, imageHeight, pixelFormat );
+}
+
+// Not needed: ?
+///-----/**
+///----- * A convenience function for creating the common case of an uncompressed image
+///----- * having exactly width * height pixels in the buffer with no padding bytes at
+///----- * row ends and no extra rows.
+///----- * @param[in] imageBuffer A buffer of pixel data that should have been allocated using <code>new uint8_t[]</code>.
+///----- * @param[in] imageWidth The width in pixels of the image.
+///----- * @param[in] imageHeight The height in pixels of the image.
+///----- * @param[in] pixelFormat The format of the image data in the buffer.
+///----- */
+///-----inline ImageDataPtr NewBitmapImageData( uint8_t * imageBuffer, unsigned imageWidth, unsigned imageHeight, Pixel::Format pixelFormat )
+///-----{
+///-----  DALI_ASSERT_DEBUG( pixelFormat <= Pixel::BGRA8888 && "Pixel format must be an addressable one." );
+///-----  const size_t numBytes = imageWidth * imageHeight * Pixel::GetBytesPerPixel( pixelFormat );
+///-----
+///-----  return ImageData::New( imageBuffer, numBytes, imageWidth, imageHeight, pixelFormat );
+///-----}
+
+} // namespace Integration
+
+} // namespace Dali
+
+#endif // __DALI_INTEGRATION_IMAGE_DATA_H__
--- dali/internal/update/resources/resource-manager.cpp
+++ dali/internal/update/resources/resource-manager.cpp
@@ -119,6 +119,8 @@ struct ResourceManager::ResourceManagerImpl
     }
   }
 
+  void UploadImageData( const ResourceId id, Bitmap& bitmap );
+
   PlatformAbstraction&     mPlatformAbstraction;
   NotificationManager&     mNotificationManager;
   ResourceClient*          mResourceClient; // (needs to be a ptr - it's not instantiated yet)
@@ -496,18 +498,6 @@ void ResourceManager::HandleSaveResourceRequest( ResourceId id, const ResourceTy
 
     switch( typePath.type->id )
     {
-      case ResourceBitmap:
-      {
-        break;
-      }
-      case ResourceNativeImage:
-      {
-        break;
-      }
-      case ResourceTargetImage:
-      {
-        break;
-      }
       case ResourceShader:
       {
         resource = GetShaderData(id);
@@ -518,11 +508,15 @@ void ResourceManager::HandleSaveResourceRequest( ResourceId id, const ResourceTy
         resource = GetModelData(id);
         break;
       }
+
+      // No other resource types are saveable:
+      case ResourceImageData:
+      case ResourceNativeImage:
+      case ResourceTargetImage:
       case ResourceMesh:
-      {
-        break;
-      }
       case ResourceText:
+      case ResourceTexture:
+      case ResourceAppBitmap:
       {
         break;
       }
@@ -589,7 +583,7 @@ void ResourceManager::HandleDiscardResourceRequest( ResourceId deadId, ResourceT
 
   if (wasComplete)
   {
-    if(typeId == ResourceBitmap ||
+    if(typeId == ResourceImageData ||
        typeId == ResourceNativeImage ||
        typeId == ResourceTargetImage )
     {
@@ -738,6 +732,32 @@ LoadStatus ResourceManager::GetAtlasLoadStatus( ResourceId atlasId )
  ************************* ResourceCache Implementation  ************************
  ********************************************************************************/
 
+namespace
+{
+
+  void OnImageLoad( ResourceId id, ResourcePointer resource, LoadStatus loadStatus )
+  {
+    ;
+  }
+}
+
+void ResourceManager::ResourceManagerImpl::UploadImageData( const ResourceId id, Bitmap& bitmap )
+{
+  // Either update an existing texture for a reloaded image or allocate a fresh
+  // texture for a newly loaded image:
+  BitmapMetadataIter iter = mBitmapMetadata.find( id );
+  if (iter != mBitmapMetadata.end())
+  {
+    iter->second.Update( &bitmap );
+    mTextureCacheDispatcher.DispatchUpdateTexture( id, &bitmap );
+  }
+  else
+  {
+    mTextureCacheDispatcher.DispatchCreateTextureForBitmap( id, &bitmap );
+    mBitmapMetadata.insert( BitmapMetadataPair( id, BitmapMetadata::New(&bitmap) ) );
+  }
+}
+
 void ResourceManager::LoadResponse( ResourceId id, ResourceTypeId type, ResourcePointer resource, LoadStatus loadStatus )
 {
   DALI_ASSERT_DEBUG( mImpl->mResourceClient != NULL );
@@ -759,41 +779,27 @@ void ResourceManager::LoadResponse( ResourceId id, ResourceTypeId type, Resource
 
     switch ( type )
     {
-      case ResourceBitmap:
+      case ResourceImageData:
       {
         DALI_ASSERT_DEBUG( loadStatus == RESOURCE_COMPLETELY_LOADED && "Partial results not handled for image loading.\n" );
-        Bitmap* const bitmap = static_cast<Bitmap*>( resource.Get() );
-        if( !bitmap )
+        ImageData* const imageData = static_cast<ImageData*>( resource.Get() );
+        if( !imageData )
         {
-          DALI_LOG_ERROR( "Missing bitmap in loaded resource with id %u.\n", id );
+          DALI_LOG_ERROR( "Missing imageData in loaded resource with id %u.\n", id );
           break;
         }
-        unsigned int bitmapWidth  = bitmap->GetImageWidth();
-        unsigned int bitmapHeight = bitmap->GetImageHeight();
+        unsigned int bitmapWidth  = imageData->imageWidth;
+        unsigned int bitmapHeight = imageData->imageHeight;
 
-        if( Bitmap::PackedPixelsProfile * packedBitmap = bitmap->GetPackedPixelsProfile() )
-        {
-          bitmapWidth  = packedBitmap->GetBufferWidth();
-          bitmapHeight = packedBitmap->GetBufferHeight();
-        }
-        Pixel::Format pixelFormat = bitmap->GetPixelFormat();
+        Pixel::Format pixelFormat = imageData->pixelFormat;
 
-        ImageAttributes attrs = ImageAttributes::New( bitmapWidth, bitmapHeight, pixelFormat ); ///!< Issue #AHC01
+        ImageAttributes attrs = ImageAttributes::New( bitmapWidth, bitmapHeight, pixelFormat );
         UpdateImageTicket (id, attrs);
 
-        // Check for reloaded bitmap
-        BitmapMetadataIter iter = mImpl->mBitmapMetadata.find(id);
-        if (iter != mImpl->mBitmapMetadata.end())
-        {
-          iter->second.Update(bitmap);
-          mImpl->mTextureCacheDispatcher.DispatchUpdateTexture( id, bitmap );
-        }
-        else
-        {
-          mImpl->mTextureCacheDispatcher.DispatchCreateTextureForBitmap( id, bitmap );
-          mImpl->mBitmapMetadata.insert(BitmapMetadataPair(id, BitmapMetadata::New(bitmap)));
-        }
+        // Pass the loaded data on towards a GLES texture:
+        OwnerPointer<Bitmap> bitmap( Bitmap::New( pixelFormat ) );
 
+        mImpl->UploadImageData( id, *bitmap );
         break;
       }
 
@@ -810,11 +816,6 @@ void ResourceManager::LoadResponse( ResourceId id, ResourceTypeId type, Resource
         break;
       }
 
-      case ResourceTargetImage:
-      {
-        break;
-      }
-
       case ResourceShader:
       {
         mImpl->mShaders.insert(ShaderDataPair(id, static_cast<ShaderData*>(resource.Get())));
@@ -827,11 +828,6 @@ void ResourceManager::LoadResponse( ResourceId id, ResourceTypeId type, Resource
         break;
       }
 
-      case ResourceMesh:
-      {
-        break;
-      }
-
       case ResourceText:
       {
         /* here we return a vector of Characters (glyph data + bitmap)*/
@@ -840,6 +836,16 @@ void ResourceManager::LoadResponse( ResourceId id, ResourceTypeId type, Resource
         UploadGlyphsToTexture(*glyphSet);
         mImpl->atlasStatus.Update(id, glyphSet->GetAtlasResourceId(), loadStatus );
         SendToClient( LoadingGlyphSetSucceededMessage( *mImpl->mResourceClient, id, glyphSet, loadStatus) );
+        break;
+      }
+
+
+      case ResourceTargetImage:
+      case ResourceMesh:
+      case ResourceTexture:
+      case ResourceAppBitmap:
+      {
+        DALI_ASSERT_DEBUG( 0 == "These resource types are never loaded." );
         break;
       }
     }
@@ -970,15 +976,15 @@ void ResourceManager::UploadGlyphsToTexture( const GlyphSet& glyphSet )
     const GlyphSet::Character& character( charList[i] );
 
     // grab a pointer to the bitmap
-    Bitmap* bitmap( character.first.Get() );
+    ImageData* imageData( character.first.Get() );
 
-    // create a bitmap upload object, then add it to the array
-    BitmapUpload upload( bitmap->ReleaseBuffer(),               // Inform the bitmap we're taking ownership of it's pixel buffer.
-                         character.second.xPosition,            // x position in the texture to which upload the bitmap
-                         character.second.yPosition,            // y position in the texture to which upload the bitmap
-                         bitmap->GetImageWidth(),              // bitmap width
-                         bitmap->GetImageHeight(),             // bitmap height
-                         BitmapUpload::DISCARD_PIXEL_DATA );    // tell the the texture to delete the bitmap pixel buffer when it's done
+    // create a ImageData upload object, then add it to the array
+    BitmapUpload upload( imageData->ReleaseImageBuffer(),  // Inform the ImageData we're taking ownership of it's pixel buffer.
+                         character.second.xPosition,         // x position in the texture to which upload the ImageData
+                         character.second.yPosition,         // y position in the texture to which upload the ImageData
+                         imageData->imageWidth,              // ImageData width
+                         imageData->imageHeight,             // ImageData height
+                         BitmapUpload::DISCARD_PIXEL_DATA ); // Tell the the texture to delete the ImageData pixel buffer when it's done
 
     uploadArray.push_back( upload );
   }
@@ -1047,14 +1053,19 @@ void ResourceManager::DiscardDeadResources( BufferIndex updateBufferIndex )
      */
     switch (iter->second)
     {
-      case ResourceBitmap:
+      case ResourceImageData:
       case ResourceNativeImage:
       case ResourceTargetImage:
+      case ResourceText:
+      case ResourceTexture:
+      case ResourceAppBitmap:
+      {
         break;
+      }
 
       case ResourceModel:
       {
-        ModelCacheIter model = mImpl->mModels.find(iter->first);
+        ModelCacheIter model = mImpl->mModels.find( iter->first );
         DALI_ASSERT_DEBUG( mImpl->mModels.end() != model );
 
         mImpl->mDiscardQueue.Add( updateBufferIndex, *(model->second) );
@@ -1072,14 +1083,9 @@ void ResourceManager::DiscardDeadResources( BufferIndex updateBufferIndex )
       }
       break;
 
-      case ResourceText:
-      {
-        break;
-      }
-
       case ResourceShader:
       {
-        ShaderCacheIter shaderIter = mImpl->mShaders.find(iter->first);
+        ShaderCacheIter shaderIter = mImpl->mShaders.find( iter->first );
         DALI_ASSERT_DEBUG( mImpl->mShaders.end() != shaderIter );
 
         mImpl->mDiscardQueue.Add( updateBufferIndex, *(shaderIter->second) );
@@ -1088,10 +1094,10 @@ void ResourceManager::DiscardDeadResources( BufferIndex updateBufferIndex )
       }
     }
 
-    mImpl->atlasStatus.Remove(iter->first);
+    mImpl->atlasStatus.Remove( iter->first );
 
     // Erase the item and increment the iterator
-    mImpl->deadRequests.erase(iter++);
+    mImpl->deadRequests.erase( iter++ );
   }
 }
 

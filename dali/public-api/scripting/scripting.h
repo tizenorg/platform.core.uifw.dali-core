c21788bf7e1cd36a99d5f44057b338a033d1f967
Adeel Kazmi <adeel.kazmi@samsung.com>
2014-03-19 18:57:21 +0000

    (Scripting) Added ability to create a new actor using a Property::Map and fixed shader property
    [Problem]  1) Cannot create an actor from a Property::Map.
               2) ShaderEffect not working when setting via properties.
    [Cause]    1) Not available.
               2) Not creating a program if geometry-type property not provided.
    [Solution] 1) Create a method which creates an actor from a property map and vice versa.
               2) Use default geometry-type if none specified.
    
    Change-Id: I1866a82a818100dafcdb3d9ba9952fbf00c6606e
    Signed-off-by: Adeel Kazmi <adeel.kazmi@samsung.com>

--- dali/public-api/scripting/scripting.h
+++ dali/public-api/scripting/scripting.h
@@ -18,12 +18,16 @@
 //
 
 // INTERNAL INCLUDES
+#include <dali/public-api/actors/actor-enumerations.h>
+#include <dali/public-api/actors/draw-mode.h>
 #include <dali/public-api/images/image.h>
 #include <dali/public-api/shader-effects/shader-effect.h>
 #include <dali/public-api/object/property-value.h>
 
 namespace Dali DALI_IMPORT_API
 {
+
+class Actor;
 
 /**
  * @brief Utilities for scripting support.
@@ -65,55 +69,125 @@ bool SetIfEqual(const std::string& a, const std::string& b, T& set, T value)
   }
 }
 
+/**
+ * @brief Takes a string and returns the appropriate color mode.
+ *
+ * @param[in] value The input string
+ * @return The corresponding color-mode.
+ */
+ColorMode GetColorMode( const std::string& value );
+
+/**
+ * @brief Takes a string and returns the appropriate position inheritance mode.
+ *
+ * @param[in] value The input string
+ * @return The corresponding position-inheritance-mode.
+ */
+PositionInheritanceMode GetPositionInheritanceMode( const std::string& value );
+
+/**
+ * @brief Takes a string and returns the appropriate draw mode.
+ *
+ * @param[in] value The input string
+ * @return The corresponding draw-mode.
+ */
+DrawMode::Type GetDrawMode( const std::string& value );
+
+/**
+ * @brief Takes a string and returns the appropriate anchor-point or parent-origin constant.
+ *
+ * @param[in] value The input string
+ * @return The corresponding anchor-point or parent-origin constant.
+ */
+Vector3 GetAnchorConstant( const std::string& value );
 
 /**
  * @brief Creates object with data from the property value map.
  *
- * @param [in] map The property value map with the following valid fields
- *                 "filename":       type STRING
- *                 "load-policy"     type STRING (enum)
- *                 "release-policy"  type STRING (enum)
- *                 "width"           type FLOAT
- *                 "height"          type FLOAT
- *                 "pixel-format"    type STRING (enum)
- *                 "scaling-mode"    type STRING (enum)
- *                 "crop"            type VECTOR4
- *                 "type"            type STRING (FrameBufferImage|BitMapImage|Image(default))
- *             Some fields are optional and some only pertain to a specific type.
+ * @param[in] map The property value map with the following valid fields:
+ * @code
+ * "filename":       type STRING
+ * "load-policy"     type STRING (enum)
+ * "release-policy"  type STRING (enum)
+ * "width"           type FLOAT
+ * "height"          type FLOAT
+ * "pixel-format"    type STRING (enum)
+ * "scaling-mode"    type STRING (enum)
+ * "crop"            type VECTOR4
+ * "type"            type STRING (FrameBufferImage|BitMapImage|Image(default))
+ * @endcode
+ * Some fields are optional and some only pertain to a specific type.
+ *
  * @return a pointer to a newly created object.
  */
 Image NewImage( const Property::Value& map );
 
-
 /**
  * @brief Creates object with data from the property value map.
  *
- * @param [in] map The property value map with the following valid fields
- *                 // a program can be specified as string or a filename.
- *                 // some fields may be ignored depending on the geometry-type
- *                 "program":        type MAP
- *                 { "vertex":                  type STRING
- *                   "fragment":                type STRING
- *                   "vertex-prefix":           type STRING
- *                   "fragment-prefix":         type STRING
- *                   "text-vertex":             type STRING
- *                   "text-fragment":           type STRING
- *                   "vertex-filename":         type STRING
- *                   "fragment-filename":       type STRING
- *                   "vertex-prefix-filename":  type STRING
- *                   "fragment-prefix-filename":type STRING
- *                   "text-vertex-filename":    type STRING
- *                   "text-fragment-filename":  type STRING
- *                   "geometry-type":           type STRING (enum)
- *                   "geometry-hints":          type STRING (enum)
- *                 }
- *                 // uniforms must be specified to be registered
- *                 "uUniform1":       type FLOAT,
- *                 "uUniform2":       type FLOAT,  etc
+ * @param[in] map The property value map with the following valid fields:
+ * @code
+ * // a program can be specified as string or a filename.
+ * // some fields may be ignored depending on the geometry-type
+ * "program":        type MAP
+ * {
+ *   "vertex":                  type STRING
+ *   "fragment":                type STRING
+ *   "vertex-prefix":           type STRING
+ *   "fragment-prefix":         type STRING
+ *   "text-vertex":             type STRING
+ *   "text-fragment":           type STRING
+ *   "vertex-filename":         type STRING
+ *   "fragment-filename":       type STRING
+ *   "vertex-prefix-filename":  type STRING
+ *   "fragment-prefix-filename":type STRING
+ *   "text-vertex-filename":    type STRING
+ *   "text-fragment-filename":  type STRING
+ *   "geometry-type":           type STRING (enum)
+ *   "geometry-hints":          type STRING (enum)
+ * }
+ * // uniforms must be specified to be registered
+ * "uUniform1":       type FLOAT,
+ * "uUniform2":       type FLOAT,  etc
+ * @endcode
  *
  * @return a pointer to a newly created object.
  */
 ShaderEffect NewShaderEffect( const Property::Value& map );
+
+/**
+ * @brief Creates an actor with the date from the property value map.
+ *
+ * @param[in] map The property value map with the properties (and hierarchy) of the actor required
+ *                 For example:
+ * @code
+ * {
+ *   "type": "ImageActor",
+ *   "image":
+ *   {
+ *     "filename":"my-image-path.png"
+ *   },
+ *   "actors":
+ *   [
+ *     {
+ *       "type":"Actor",
+ *       "position":[0,0,0]
+ *     }
+ *   ]
+ * }
+ * @endcode
+ *
+ * @return Handle to the newly created actor.
+ */
+Actor NewActor( const Property::Map& map );
+
+/**
+ * @brief Creates a Property::Map from the actor provided.
+ *
+ * @param[in] actor The base-actor from which a Property::Map should be created
+ * @param[out] map This map is cleared and a property map of actor and its children is filled in
+ */
+void CreatePropertyMap( Actor actor, Property::Map& map );
 
 }
 

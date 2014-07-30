b6c00f2536789790e42bbccb85b22f1b17c8222b
Adeel Kazmi <adeel.kazmi@samsung.com>
2014-07-30 16:41:55 +0100

    Add some constants that show the version of the library
    Change-Id: If92845f47af4d29bdd0fc395b5e49f8772c7d541

---
+++ dali/public-api/dali-core-version.h
@@ -0,0 +1,28 @@
+#ifndef __DALI_CORE_VERSION_H__
+#define __DALI_CORE_VERSION_H__
+
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+namespace Dali
+{
+extern const unsigned int CORE_MAJOR_VERSION;
+extern const unsigned int CORE_MINOR_VERSION;
+extern const unsigned int CORE_MICRO_VERSION;
+} // unnamed namespace
+
+#endif // __DALI_CORE_VERSION_H__

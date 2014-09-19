6054ea02137d7f01a7589bfe4e942ea97cac8c33
Richard Huang <r.huang@samsung.com>
2014-09-19 11:41:01 +0100

    Added hover event in Dali
    Hover events are a collection of touch points at a specific moment in time.
    When a multi-touch event occurs, each touch point represents the points that
    are currently being hovered or the points where a hover has stopped.
    
    Hover event processor processes hover events and emits the Hovered signal
    on the hit actor (and its parents).
    
    Change-Id: Ife8e7c1561158e8f98061eb5037ad3b6944846dd

---
+++ dali/integration-api/events/hover-event-integ.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/integration-api/events/hover-event-integ.h>
+
+// INTERNAL INCLUDES
+#include <dali/public-api/common/dali-common.h>
+
+namespace Dali
+{
+
+namespace Integration
+{
+
+HoverEvent::HoverEvent()
+: Event(Hover),
+  time(0)
+{
+}
+
+HoverEvent::HoverEvent(unsigned long time)
+: Event(Hover),
+  time(time)
+{
+}
+
+HoverEvent::~HoverEvent()
+{
+}
+
+void HoverEvent::AddPoint(const TouchPoint& point)
+{
+  points.push_back(point);
+}
+
+TouchPoint& HoverEvent::GetPoint(unsigned int point)
+{
+  DALI_ASSERT_ALWAYS(point < points.size() && "HoverEvent: Point index out of bounds");
+  return points[point];
+}
+
+const TouchPoint& HoverEvent::GetPoint(unsigned int point) const
+{
+  DALI_ASSERT_ALWAYS(point < points.size() && "HoverEvent: Point index out of bounds");
+  return points[point];
+}
+
+unsigned int HoverEvent::GetPointCount() const
+{
+  return points.size();
+}
+
+} // namespace Integration
+
+} // namespace Dali

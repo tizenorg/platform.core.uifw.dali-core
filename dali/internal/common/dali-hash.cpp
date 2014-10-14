13b06fb77777381072506d0c5f898c229a709fe2
Nick Holland <nick.holland@partner.samsung.com>
2014-10-14 08:17:35 +0100

    Optimized shader hash to not allocate memory or perform multiple passes of string data
    Applications like dali-demo end up hashing around 1MB of shader source on startup
    due to multiple calls to ShaderFactory::Load ( 340 calls for dali-demo).
    
    The previous code would:
     - Allocate a new string to hold both vertex/ fragment source code
     - Copy vertex / fragment source code in to the new string
     - Pass through the string removing white spaces
     - Pass through the string removing tabs
     - Pass through the string removing new line characters
     - Pass through the string to perform Hash calculation
    
    So if a total of 1MB is passed to the hasher it would end
    read/ writing around 5MB of data.
    
    Hence OProfile flagged it up as being a bottleneck for some apps on startup.
    
    New version does no memory allocation and does a single pass of the data.
    
    Todo: Need to look at a better way of comparing shaders
    
    Change-Id: I39c8b351c65c6e91f2c4b32e321f861d634ccca4

--- dali/internal/common/dali-hash.cpp
+++ dali/internal/common/dali-hash.cpp
@@ -22,21 +22,43 @@
 namespace Dali
 {
 
+namespace //un-named namespace
+{
 /*
  * djb2 (http://www.cse.yorku.ca/~oz/hash.html)
  */
-unsigned long StringHash::operator()(const std::string& toHash)
+const unsigned long INITIAL_HASH_VALUE = 5381;
+
+inline void HashShader( const char* string, unsigned long& hash )
 {
-  unsigned long hash = 5381;
-
-  const char *str = toHash.c_str();
-
-  while( int c = *str++ )
+  // performs normal hash but ignores white spaces, carriage returns and tabs
+  while( int c = *string++ )
   {
     hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
   }
+}
+} // un-named namespace
+
+unsigned long StringHash::operator()(const std::string& toHash)
+{
+  unsigned long hash = INITIAL_HASH_VALUE;
+
+  HashShader( toHash.c_str(), hash );
 
   return hash;
 }
+
+unsigned long StringHashShaderCode::operator()(const std::string& fragment, const std::string& vertex)
+{
+  unsigned long hash = INITIAL_HASH_VALUE;
+
+  HashShader( fragment.c_str() , hash);
+  HashShader( vertex.c_str(), hash );
+
+  return hash;
+}
+
+
+
 
 } // namespace Dali

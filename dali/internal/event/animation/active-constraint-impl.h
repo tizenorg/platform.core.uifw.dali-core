4506f5775a48067fff574ad0474205034ef6607e
Kimmo Hoikka <kimmo.hoikka@samsung.com>
2014-11-10 17:53:42 +0000

    Remove a few exports by getting rid of two std::vectors and two std::sets
    [Problem]
    [Cause]
    [Solution]
    
    Change-Id: I84df0e7804fd732002cd09db72879b4f82c9f211

--- dali/internal/event/animation/active-constraint-impl.h
+++ dali/internal/event/animation/active-constraint-impl.h
@@ -150,8 +150,8 @@ private:
     }
 
     // Build a set of property-owners, providing the scene-graph properties
-    SceneGraph::PropertyOwnerSet propertyOwners;
-    propertyOwners.insert( targetObject );
+    SceneGraph::PropertyOwnerContainer propertyOwners;
+    propertyOwners.PushBack( targetObject );
 
     // Build the constraint function; this requires a scene-graph property from each source
     ConstraintFunctionPtr func( ConnectConstraintFunction( propertyOwners ) );
@@ -192,7 +192,7 @@ private:
    * @param[out] propertyOwners The set of property-owners providing the scene-graph properties.
    * @return A connected constraint-function, or NULL if the scene-graph properties are not available.
    */
-  PropertyConstraintBase<PropertyType>* ConnectConstraintFunction( SceneGraph::PropertyOwnerSet& propertyOwners )
+  PropertyConstraintBase<PropertyType>* ConnectConstraintFunction( SceneGraph::PropertyOwnerContainer& propertyOwners )
   {
     PropertyConstraintBase<PropertyType>* func = mUserFunction->Clone();
     bool usingComponentFunc( false );
@@ -213,7 +213,7 @@ private:
         // The property owner will not exist, if the target proxy-object is off-stage
         if( NULL != owner )
         {
-          propertyOwners.insert( owner );
+          propertyOwners.PushBack( owner );
           inputProperty = const_cast< PropertyInputImpl* >( source.object->GetSceneObjectInputProperty( source.propertyIndex ) );
           componentIndex = source.object->GetPropertyComponentIndex( source.propertyIndex );
 
@@ -247,7 +247,7 @@ private:
           // The property owner will not exist, if the parent proxy-object is off-stage
           if ( NULL != owner )
           {
-            propertyOwners.insert( owner );
+            propertyOwners.PushBack( owner );
             inputProperty = const_cast< PropertyInputImpl* >( proxyParent->GetSceneObjectInputProperty( source.propertyIndex ) );
             componentIndex = proxyParent->GetPropertyComponentIndex( source.propertyIndex );
 
@@ -397,8 +397,8 @@ private:
     }
 
     // Build a set of property-owners, providing the scene-graph properties
-    SceneGraph::PropertyOwnerSet propertyOwners;
-    propertyOwners.insert( targetObject );
+    SceneGraph::PropertyOwnerContainer propertyOwners;
+    propertyOwners.PushBack( targetObject );
 
     // Build the constraint function; this requires a scene-graph property from each source
     ConstraintFunctionPtr func( ConnectConstraintFunction( propertyOwners ) );
@@ -501,7 +501,7 @@ private:
    * @param[out] propertyOwners The set of property-owners providing the scene-graph properties.
    * @return A connected constraint-function, or NULL if the scene-graph properties are not available.
    */
-  PropertyConstraintBase<float>* ConnectConstraintFunction( SceneGraph::PropertyOwnerSet& propertyOwners )
+  PropertyConstraintBase<float>* ConnectConstraintFunction( SceneGraph::PropertyOwnerContainer& propertyOwners )
   {
     PropertyConstraintBase<float>* func = mUserFunction->Clone();
     bool usingComponentFunc( false );
@@ -522,7 +522,7 @@ private:
         // The property owner will not exist, if the target proxy-object is off-stage
         if( NULL != owner )
         {
-          propertyOwners.insert( owner );
+          propertyOwners.PushBack( owner );
           inputProperty = const_cast< PropertyInputImpl* >( source.object->GetSceneObjectInputProperty( source.propertyIndex ) );
           componentIndex = source.object->GetPropertyComponentIndex( source.propertyIndex );
 
@@ -556,7 +556,7 @@ private:
           // The property owner will not exist, if the parent proxy-object is off-stage
           if ( NULL != owner )
           {
-            propertyOwners.insert( owner );
+            propertyOwners.PushBack( owner );
             inputProperty = const_cast< PropertyInputImpl* >( proxyParent->GetSceneObjectInputProperty( source.propertyIndex ) );
             componentIndex = proxyParent->GetPropertyComponentIndex( source.propertyIndex );
 

e09b078b4c324bd41c3926941865b307b75e6307
Paul Wisbey <p.wisbey@samsung.com>
2015-01-12 16:20:49 +0000

    Minimal Atlas API
    Change-Id: I75c7a8c32b2d70fc6bf90267dee33afe4da063fe

---
+++ dali/public-api/images/atlas.cpp
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/public-api/images/atlas.h>
+
+// INTERNAL INCLUDES
+//#include <dali/internal/event/images/atlas-impl.h>
+
+namespace Dali
+{
+
+Atlas Atlas::New( std::size_t width,
+                  std::size_t height,
+                  Pixel::Format pixelformat )
+{
+  DALI_ASSERT_ALWAYS( 0u != width  && "Invalid Atlas width requested" );
+  DALI_ASSERT_ALWAYS( 0u != height && "Invalid Atlas height requested" );
+
+  return Atlas();
+//  return Atlas( Internal::Atlas::New( width, height, pixelformat ) );
+}
+
+Atlas::Atlas()
+{
+}
+
+bool Atlas::Upload( const BitmapImage& bitmap,
+                    std::size_t xOffset,
+                    std::size_t yOffset )
+{
+  return false;
+//  return GetImplementation(*this).Upload( bitmap, xOffset, yOffset );
+}
+
+Atlas Atlas::DownCast( BaseHandle handle )
+{
+  return Atlas();
+//  return Atlas( dynamic_cast<Dali::Internal::Atlas*>(handle.GetObjectPtr()) );
+}
+
+Atlas::~Atlas()
+{
+}
+
+Atlas::Atlas( const Atlas& handle )
+: Image( handle )
+{
+}
+
+Atlas& Atlas::operator=( const Atlas& rhs )
+{
+  BaseHandle::operator=(rhs);
+  return *this;
+}
+
+Atlas::Atlas( Internal::Atlas* internal )
+: Image( NULL )
+//: Image( internal )
+{
+}
+
+} // namespace Dali

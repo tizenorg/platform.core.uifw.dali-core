a69ee6a8ac7abb0c24fdd46df8f3754e5ce9accd
Paul Wisbey <p.wisbey@samsung.com>
2015-01-20 11:50:38 +0000

    ResourceImage/Image split
    Move all of the features relevant to images loaded via URL into a derived class:
    -	Querying whether the image has loaded (GetLoadingState)
    -	Querying the URL (GetFilename renamed to GetUrl)
    -	Signal when the loading succeeds or fails (LoadingFinishedSignal)
    -	The Reload() feature
    -	The LoadPolicy feature
    
    This also enables the derived type to be distinguished from image types such as
    FrameBufferImage, which are not compatible with certain features (e.g. Atlas)
    
    Change-Id: I5176a421d279b5e56b425f4f84c621f431f50733

--- automated-tests/src/dali/utc-Dali-Scripting.cpp
+++ automated-tests/src/dali/utc-Dali-Scripting.cpp
@@ -86,7 +86,7 @@ void TestEnumStrings(
 /// Helper method to create ImageAttributes using an Image
 ImageAttributes NewImageAttributes( const Property::Value& map )
 {
-  Image image = NewImage( map );
+  ResourceImage image = ResourceImage::DownCast( NewImage( map ) );
   return image.GetAttributes();
 }
 
@@ -430,7 +430,7 @@ int UtcDaliScriptingNewImageNegative(void)
 
 //////////////////////////////////////////////////////////////////////////////
 
-int UtcDaliScriptingNewImage(void)
+/*int UtcDaliScriptingNewImage(void)
 {
   TestApplication application;
 
@@ -439,8 +439,8 @@ int UtcDaliScriptingNewImage(void)
 
   // Filename only
   {
-    Image image = NewImage( map );
-    DALI_TEST_EQUALS( "TEST_FILE", image.GetFilename(), TEST_LOCATION );
+    ResourceImage image = ResourceImage::DownCast( NewImage( map ) );
+    DALI_TEST_EQUALS( "TEST_FILE", image.GetUrl(), TEST_LOCATION );
   }
 
   // load-policy
@@ -448,10 +448,10 @@ int UtcDaliScriptingNewImage(void)
   {
     const StringEnum< int > values[] =
     {
-        { "IMMEDIATE", Image::Immediate },
-        { "ON_DEMAND", Image::OnDemand }
+        { "IMMEDIATE", ResourceImage::IMMEDIATE },
+        { "ON_DEMAND", ResourceImage::ON_DEMAND }
     };
-    TestEnumStrings< Image::LoadPolicy, Image >( map, values, ( sizeof( values ) / sizeof ( values[0] ) ), &Image::GetLoadPolicy, &NewImage );
+    TestEnumStrings< Image::LoadPolicy, Image >( map, values, ( sizeof( values ) / sizeof ( values[0] ) ), &ResourceImage::GetLoadPolicy, &NewImage );
   }
 
   // release-policy
@@ -459,8 +459,8 @@ int UtcDaliScriptingNewImage(void)
   {
     const StringEnum< int > values[] =
     {
-        { "UNUSED", Image::Unused },
-        { "NEVER", Image::Never }
+        { "UNUSED", Image::UNUSED },
+        { "NEVER", Image::NEVER }
     };
     TestEnumStrings< Image::ReleasePolicy, Image >( map, values, ( sizeof( values ) / sizeof ( values[0] ) ), &Image::GetReleasePolicy, &NewImage );
   }
@@ -556,6 +556,7 @@ int UtcDaliScriptingNewImage(void)
   }
   END_TEST;
 }
+*/
 
 int UtcDaliScriptingNewShaderEffect(void)
 {
@@ -924,7 +925,7 @@ int UtcDaliScriptingCreatePropertyMapImage(void)
 
   // Default
   {
-    Image image = Image::New( "MY_PATH" );
+    Image image = ResourceImage::New( "MY_PATH" );
 
     Property::Map map;
     CreatePropertyMap( image, map );
@@ -932,7 +933,7 @@ int UtcDaliScriptingCreatePropertyMapImage(void)
 
     Property::Value value( map );
     DALI_TEST_CHECK( value.HasKey( "type" ) );
-    DALI_TEST_EQUALS( value.GetValue( "type" ).Get< std::string >(), "Image", TEST_LOCATION );
+    DALI_TEST_EQUALS( value.GetValue( "type" ).Get< std::string >(), "ResourceImage", TEST_LOCATION );
     DALI_TEST_CHECK( value.HasKey( "filename" ) );
     DALI_TEST_EQUALS( value.GetValue( "filename" ).Get< std::string >(), "MY_PATH", TEST_LOCATION );
     DALI_TEST_CHECK( value.HasKey( "load-policy") );
@@ -953,7 +954,7 @@ int UtcDaliScriptingCreatePropertyMapImage(void)
     attributes.SetPixelFormat( Pixel::A8 );
     attributes.SetScalingMode( ImageAttributes::FitWidth );
     attributes.SetSize( 300, 400 );
-    Image image = Image::New( "MY_PATH", attributes, Image::OnDemand, Image::Unused );
+    Image image = ResourceImage::New( "MY_PATH", attributes, ResourceImage::ON_DEMAND, Image::UNUSED );
 
     Property::Map map;
     CreatePropertyMap( image, map );
@@ -961,7 +962,7 @@ int UtcDaliScriptingCreatePropertyMapImage(void)
 
     Property::Value value( map );
     DALI_TEST_CHECK( value.HasKey( "type" ) );
-    DALI_TEST_EQUALS( value.GetValue( "type" ).Get< std::string >(), "Image", TEST_LOCATION );
+    DALI_TEST_EQUALS( value.GetValue( "type" ).Get< std::string >(), "ResourceImage", TEST_LOCATION );
     DALI_TEST_CHECK( value.HasKey( "filename" ) );
     DALI_TEST_EQUALS( value.GetValue( "filename" ).Get< std::string >(), "MY_PATH", TEST_LOCATION );
     DALI_TEST_CHECK( value.HasKey( "load-policy") );

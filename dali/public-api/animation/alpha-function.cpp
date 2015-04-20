6ca9432782ec3da4017685961bf144ade704681a
Ferran Sole <ferran.sole@samsung.com>
2015-04-20 14:35:58 +0100

    AlphaFunction refactoring
    Removed all the alpha functions from public api. Alpha functions can now be
    defined using an enum to use one of the predefined alpha functions, a pointer,
    to define a custom alpha function, or two Vector2 which are used for cubic bezier
    alpha function.
    
    The new API is used as follows:
    
    //Using a predefined alpha function
    animation.AnimateTo( property, value, AlphaFunction::EASE_IN_OUT );
    
    //Using a custom alpha function
    animation.AnimateTo( property, value, &my_alpha_function );
    
    //Using a bezier alpha function
    animation.AnimateTo( property, value, AlphaFunction(Vector2(0.0f,1.0f),Vector2(1.0f,0.0f) ) );
    
    Change-Id: I4051c752d826770447923b6e9a3215f2575d6370

---
+++ dali/public-api/animation/alpha-function.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/public-api/animation/alpha-function.h>
+
+// INTERNAL INCLUDES
+
+namespace Dali
+{
+
+AlphaFunction::AlphaFunction()
+:mBezierControlPoints(Vector4::ZERO),
+ mCustom(0),
+ mBuiltin(DEFAULT),
+ mMode(BUILTIN_FUNCTION)
+{}
+
+AlphaFunction::AlphaFunction( BuiltinFunction function)
+:mBezierControlPoints(Vector4::ZERO),
+ mCustom(0),
+ mBuiltin(function),
+ mMode(BUILTIN_FUNCTION)
+{}
+
+AlphaFunction::AlphaFunction( AlphaFunctionPrototype function)
+:mBezierControlPoints(Vector4::ZERO),
+ mCustom(function),
+ mBuiltin(DEFAULT),
+ mMode(CUSTOM_FUNCTION)
+{}
+
+AlphaFunction::AlphaFunction( const Vector2& cp0, const Vector2& cp1 )
+:mBezierControlPoints(Vector4(Clamp(cp0.x,0.0f,1.0f),cp0.y,Clamp(cp1.x,0.0f,1.0f),cp1.y)),
+ mCustom(0),
+ mBuiltin(DEFAULT),
+ mMode(BEZIER)
+{
+}
+
+Vector4 AlphaFunction::GetBezierControlPoints() const
+{
+  return mBezierControlPoints;
+}
+
+AlphaFunctionPrototype AlphaFunction::GetCustomFunction() const
+{
+  return mCustom;
+}
+
+AlphaFunction::BuiltinFunction AlphaFunction::GetBuiltinFunction() const
+{
+  return mBuiltin;
+}
+
+AlphaFunction::Mode AlphaFunction::GetMode() const
+{
+  return mMode;
+}
+
+} // namespace Dali

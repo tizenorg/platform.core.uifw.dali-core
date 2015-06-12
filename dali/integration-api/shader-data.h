e2b71fc3e0da3c23f34f9605d7dbb68af25a71c2
Andrew Cox <andrew.cox@partner.samsung.com>
2015-06-12 20:23:55 +0100

    Shader binary synchronous simplified IO
    Loading shader binaries synchronously on event thread.
    Saving shader binaries from ShaderFactory on event thread.
    
    Change-Id: I38a79a7b4e9eb33d9a5059f1c43d9e8c892b0867
    Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

--- dali/integration-api/shader-data.h
+++ dali/integration-api/shader-data.h
@@ -23,7 +23,7 @@
 
 // INTERNAL INCLUDES
 #include <dali/public-api/object/ref-object.h>
-#include <dali/public-api/common/vector-wrapper.h>
+#include <dali/public-api/common/dali-vector.h>
 #include <dali/integration-api/resource-declarations.h>
 
 namespace Dali
@@ -36,9 +36,11 @@ class ShaderData;
 
 typedef IntrusivePtr<ShaderData> ShaderDataPtr;
 
+const size_t DALI_UNINITIALISED_HASH_VALUE = size_t(0) - 1;
+
 /**
  * ShaderData class.
- * A container for shader source code and compiled binary byte code
+ * A container for shader source code and compiled binary byte code.
  */
 class ShaderData : public Dali::RefObject
 {
@@ -49,7 +51,7 @@ public:
    * @param[in] fragmentSource Source code for fragment program
    */
   ShaderData(const std::string& vertexSource, const std::string& fragmentSource)
-  : mShaderHash( 0 ),
+  : mShaderHash( DALI_UNINITIALISED_HASH_VALUE ),
     mVertexShader(vertexSource),
     mFragmentShader(fragmentSource),
     mResourceId( 0 )
@@ -73,6 +75,7 @@ public: // API
    */
   void SetHashValue(size_t shaderHash)
   {
+    DALI_ASSERT_DEBUG( shaderHash != 0 );
     mShaderHash = shaderHash;
   }
 
@@ -80,15 +83,16 @@ public: // API
    * Get hash value which is created with vertex and fragment shader code
    * @return shaderHash  hash key created with vertex and fragment shader code
    */
-  size_t GetHashValue()
+  size_t GetHashValue() const
   {
+    DALI_ASSERT_DEBUG( mShaderHash != DALI_UNINITIALISED_HASH_VALUE );
     return mShaderHash;
   }
 
   /**
    * @return the vertex shader
    */
-  const char* GetVertexShader()
+  const char* GetVertexShader() const
   {
     return mVertexShader.c_str();
   }
@@ -96,7 +100,7 @@ public: // API
   /**
    * @return the vertex shader
    */
-  const char* GetFragmentShader()
+  const char* GetFragmentShader() const
   {
     return mFragmentShader.c_str();
   }
@@ -107,7 +111,7 @@ public: // API
    */
   bool HasBinary() const
   {
-    return 0 != mBuffer.size();
+    return 0 != mBuffer.Size();
   }
 
   /**
@@ -116,19 +120,16 @@ public: // API
    */
   void AllocateBuffer( size_t size )
   {
-    if( size > mBuffer.size() )
-    {
-      mBuffer.resize( size );
-    }
+    mBuffer.Resize( size );
   }
 
   /**
    * Get the program buffer
    * @return reference to the buffer
    */
-  size_t GetBufferSize()
+  size_t GetBufferSize() const
   {
-    return mBuffer.size();
+    return mBuffer.Size();
   }
 
   /**
@@ -137,16 +138,15 @@ public: // API
    */
   unsigned char* GetBufferData()
   {
-    DALI_ASSERT_DEBUG( mBuffer.size() > 0 );
+    DALI_ASSERT_DEBUG( mBuffer.Size() > 0 );
     return &mBuffer[0];
   }
 
   /**
    * Get the data that the buffer points to
-   * @TODO TO BE REMOVED WHEN PLATFORM ABSTRACTION FOR SHADER LOAD/STORE IS FIXED
    * @return raw pointer to the buffer data
    */
-  std::vector<unsigned char>& GetBuffer()
+  Dali::Vector<unsigned char>& GetBuffer()
   {
     return mBuffer;
   }
@@ -164,7 +164,7 @@ public: // API
    * Get the resource id
    * @return resourceId
    */
-  ResourceId GetResourceId()
+  ResourceId GetResourceId() const
   {
     return mResourceId;
   }
@@ -179,9 +179,8 @@ private: // Data
   size_t                      mShaderHash;     ///< hash key created with vertex and fragment shader code
   std::string                 mVertexShader;   ///< source code for vertex program
   std::string                 mFragmentShader; ///< source code for fragment program
-  std::vector<unsigned char>  mBuffer;         ///< buffer containing compiled binary bytecode
+  Dali::Vector<unsigned char> mBuffer;         ///< buffer containing compiled binary bytecode
   ResourceId                  mResourceId;     ///< resource id
-
 };
 
 } // namespace Integration
--- dali/internal/event/effects/shader-factory.h
+++ dali/internal/event/effects/shader-factory.h
@@ -22,9 +22,19 @@
 #include <dali/internal/event/resources/resource-ticket.h>
 #include <dali/internal/event/resources/resource-type-path-id-map.h>
 #include <dali/internal/event/effects/shader-declarations.h>
+#include <dali/internal/common/message.h>
+#include <dali/internal/common/shader-dispatcher.h>
+#include <dali/integration-api/shader-data.h>
 
 namespace Dali
 {
+namespace Integration
+{
+
+class ShaderData;
+typedef IntrusivePtr<ShaderData> ShaderDataPtr;
+
+}
 
 namespace Internal
 {
@@ -32,13 +42,16 @@ namespace Internal
 class ResourceClient;
 class MessageController;
 
+const int DALI_SHADER_CACHE_SIZE = 20;
+const int DALI_SHADER_CACHE_MAX = (DALI_SHADER_CACHE_SIZE - 1);
+
 /**
  * ShaderFactory is an object that manages shader binary resource load requests,
  * It triggers the load requests during core initialization and sends a message to the
  * render manager with information about all the requested shader binaries and their
  * request Id.
  */
-class ShaderFactory
+class ShaderFactory : public ShaderDispatcher
 {
 public:
 
@@ -56,12 +69,39 @@ public:
    * Issues a request to load a binary version of a shader program, and returns a resource ticket
    * If a request for an identical shader has already been made, the ticket for the older request
    * is shared.
+   * @deprecated Use AttemptCachedLoadImmediate instead of this. Only here to support legacy ShaderEffect.
    * @param [in] vertexSource   The vertex shader source code
    * @param [in] fragmentSource The fragment shader source code
-   * @param [out] shaderHash  hash key created with vertex and fragment shader code
+   * @param [out] shaderHash    Hash key created with vertex and fragment shader code
    * @return                    A ticket for the resource
    */
   ResourceTicketPtr Load( const std::string& vertexSource, const std::string& fragmentSource, size_t& shaderHash );
+
+  /**
+   * @brief Looks for precompiled binary version of shader program in memory and file caches.
+   *
+   * Tries to load a binary version of a shader program identified by a hash over the two source
+   * files, checking an in-memory cache first.
+   * If the cache hits or the load succeeds, the buffer member of the returned ShaderData will
+   * contain a precompiled shader binary program which can be uploaded directly to GLES.
+   *
+   * @param [in] vertexSource   The vertex shader source code
+   * @param [in] fragmentSource The fragment shader source code
+   * @param [out] shaderHash    Hash key created from vertex and fragment shader code
+   * @return                    ShaderData containing the source and hash value, and additionally,
+   *                            a compiled shader program binary if one could be found, else an
+   *                            empty binary buffer cleared to size zero.
+   */
+  Integration::ShaderDataPtr AttemptCachedLoadImmediate( const std::string& vertexSource, const std::string& fragmentSource, size_t& shaderHash );
+
+  /**
+   * @brief Saves shader to memory cache and filesystem.
+   * This is called when a shader binary is ready to be saved to the memory cache file system.
+   * Shaders that pass through here become available to subsequent invocations of AttemptCachedLoadImmediate.
+   * @param[in] shader The data to be saved.
+   * @sa AttemptCachedLoadImmediate
+   */
+  virtual void Dispatch( Integration::ShaderDataPtr shader );
 
   /**
    * Called during Core initialization to load the default shader.
@@ -69,6 +109,10 @@ public:
   void LoadDefaultShaders();
 
 private:
+
+  ResourceTicketPtr GetShaderTicket( const ResourceTypePath& typePath );
+
+  void MemoryCacheInsert( Integration::ShaderDataPtr shaderData );
 
   // Undefined
   ShaderFactory( const ShaderFactory& );
@@ -80,8 +124,16 @@ private:
   ResourceClient&       mResourceClient;
   ResourceTypePathIdMap mResourceTypePathIdMap; ///< A map of resource IDs sorted by ResourceTypePath
   ShaderEffectPtr       mDefaultShader;
+  std::pair< Integration::ShaderDataPtr, size_t > mShaderBinaryCache[DALI_SHADER_CACHE_SIZE]; ///< Cache of pre-compiled shaders.
 
 }; // class ShaderFactory
+
+inline MessageBase* ShaderCompiledMessage( ShaderDispatcher& factory, Integration::ShaderDataPtr shaderData )
+{
+  return new MessageValue1< ShaderDispatcher, Integration::ShaderDataPtr >( &factory,
+                                                            &ShaderDispatcher::Dispatch,
+                                                            shaderData );
+}
 
 } // namespace Internal
 

9ee86e39654045b865cb3b242c485790029e660d
Ferran Sole <ferran.sole@samsung.com>
2015-06-29 17:27:15 +0100

    Added api to check actor's depth in the hierarchy
    Change-Id: Iafa09ddf3fb0c1507693330714a3675d9c9f6f61

--- dali/public-api/actors/actor.h
+++ dali/public-api/actors/actor.h
@@ -1260,6 +1260,13 @@ public:
    */
   Vector2 GetMaximumSize();
 
+  /**
+   * @brief Get depth in the hierarchy for the actor
+   *
+   * @return The current depth in the hierarchy of the actor, or 0 if actor is not in the hierarchy
+   */
+  unsigned int GetHierarchyDepth();
+
 public: // Signals
 
   /**

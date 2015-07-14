af1615c365a37a96f414d12648a11855b6e7d0d5
Andrew Cox <andrew.cox@partner.samsung.com>
2015-07-14 16:44:18 +0100

    Added a ScopedUnlock to Dali::Mutex and a GetMutex() to the helpers
    This is to enable a ConditionalWait::Wait( Mutex& ).
    
    Change-Id: Id2865836690a09d7a1fb2385f9a0de0276066484
    Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

--- dali/devel-api/common/mutex.cpp
+++ dali/devel-api/common/mutex.cpp
@@ -63,4 +63,26 @@ Mutex::ScopedLock::~ScopedLock()
   pthread_mutex_unlock( &mMutex.mImpl->mutex );
 }
 
+Mutex::ScopedUnlock::ScopedUnlock( Mutex& mutex )
+: mMutex( mutex ), mBeenUnlocked( false )
+{
+}
+
+void Mutex::ScopedUnlock::Unlock()
+{
+  DALI_ASSERT_DEBUG( mMutex.mImpl->locked == true );
+  mMutex.mImpl->locked = false;
+  pthread_mutex_unlock( &mMutex.mImpl->mutex );
+}
+
+Mutex::ScopedUnlock::~ScopedUnlock()
+{
+  if( mBeenUnlocked )
+  {
+    DALI_ASSERT_DEBUG( mMutex.mImpl->locked == false );
+    pthread_mutex_lock( &mMutex.mImpl->mutex );
+    mMutex.mImpl->locked = true;
+  }
+}
+
 } // namespace Dali

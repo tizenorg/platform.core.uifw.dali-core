1f826927d99a3308ecf838665cf48bff35ed9c51
Adeel Kazmi <adeel.kazmi@samsung.com>
2016-04-15 13:56:54 +0100

    (Automated Tests) Use Renderers instead of ImageActor
    Change-Id: I4cae4197b1deb40b552de225becadab3370ecc3c

--- automated-tests/src/dali/dali-test-suite-utils/mesh-builder.h
+++ automated-tests/src/dali/dali-test-suite-utils/mesh-builder.h
@@ -1,7 +1,7 @@
 #ifndef MESH_BUILDER_H
 #define MESH_BUILDER_H
 /*
- * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

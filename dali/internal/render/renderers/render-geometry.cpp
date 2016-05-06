12757d600d1f2e58b14dde35184b6839ef12731c
Adam Bialogonski <adam.b@samsung.com>
2016-05-06 17:29:43 +0100

    Fixes an issue when element buffer count is 0 in render-geometry.
    - elementBufferCount = 0 states for whole or remaining buffer from elementBufferOffset.
    
    Added two test cases covering that issue ( going out of bounds of the index buffer andusing 0 as number of elements )
    
    Change-Id: I5d29401a3c2d717d173ce63a9cc9ba3b2ecb0e64

--- dali/internal/render/renderers/render-geometry.cpp
+++ dali/internal/render/renderers/render-geometry.cpp
@@ -158,7 +158,7 @@ void Geometry::UploadAndDraw(
   }
 
   GLenum geometryGLType(0);
-  unsigned int numIndices(0u);
+  size_t numIndices(0u);
   intptr_t firstIndexOffset(0u);
   if( mIndexBuffer )
   {
@@ -166,12 +166,14 @@ void Geometry::UploadAndDraw(
 
     if( elementBufferOffset )
     {
-      elementBufferOffset = elementBufferOffset >= numIndices ? numIndices- 1 : elementBufferOffset;
+      elementBufferOffset = elementBufferOffset >= numIndices ? numIndices - 1 : elementBufferOffset;
       firstIndexOffset = elementBufferOffset * sizeof(GLushort);
     }
-    if ( elementBufferCount )
+
+    numIndices = numIndices - elementBufferOffset;
+    if( elementBufferCount )
     {
-      numIndices = std::min( elementBufferCount, numIndices - elementBufferOffset );
+      numIndices = std::min( elementBufferCount, numIndices );
     }
   }
 

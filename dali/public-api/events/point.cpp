46e555fdcd36373425fa25691dee6941dc9860bb
Adeel Kazmi <adeel.kazmi@samsung.com>
2016-05-19 18:18:33 +0100

    (Touch) Added classes to eventually replace TouchEvent/Point
    Change-Id: I3d12e4f1a041b8a323b5039ea2ee32bc4b019ceb

---
+++ dali/public-api/events/point.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// CLASS HEADER
+#include <dali/public-api/events/point.h>
+
+// INTERNAL INCLUDES
+#include <dali/public-api/common/vector-wrapper.h>
+#include <dali/public-api/actors/actor.h>
+#include <dali/public-api/math/vector2.h>
+
+namespace Dali
+{
+
+struct Point::Impl
+{
+  Actor     hitActor;     ///< The actor that was underneath the point.
+  Vector2   screen;       ///< The co-ordinates relative to the top-left of the screen.
+  Vector2   local;        ///< The co-ordinates relative to the top-left of the hit-actor.
+  State     state;        ///< State of the point.
+  int       deviceId;     ///< Each point has a unique device ID which specifies the device used for that point.
+};
+
+Point::Point( int deviceId, State state, Vector2 screen )
+: mImpl( new Impl )
+{
+  mImpl->deviceId = deviceId;
+  mImpl->screen = mImpl->local = screen;
+  mImpl->state = state;
+}
+
+Point::Point( int deviceId, State state, Vector2 screen, Vector2 local )
+: mImpl( new Impl )
+{
+  mImpl->deviceId = deviceId;
+  mImpl->state = state;
+  mImpl->screen = screen;
+  mImpl->local = local;
+}
+
+Point::Point( const Point& other )
+: mImpl( new Impl )
+{
+  mImpl = other.mImpl;
+}
+
+Point::~Point()
+{
+  delete mImpl;
+}
+
+Point& Point::operator=( const Point& other )
+{
+  if( this != &other )
+  {
+    mImpl = other.mImpl;
+  }
+
+  return *this;
+}
+
+int Point::GetDeviceId() const
+{
+  return mImpl->deviceId;
+}
+
+Point::State Point::GetState() const
+{
+  return mImpl->state;
+}
+
+
+Actor Point::GetHitActor() const
+{
+  return mImpl->hitActor;
+}
+
+Vector2 Point::GetLocal() const
+{
+  return mImpl->local;
+}
+
+Vector2 Point::GetScreen() const
+{
+  return mImpl->screen;
+}
+
+void Point::SetDeviceId( int deviceId )
+{
+  mImpl->deviceId = deviceId;
+}
+
+void Point::SetState( State state )
+{
+  mImpl->state = state;
+}
+
+void Point::SetHitActor( Actor actor )
+{
+  mImpl->hitActor = actor;
+}
+
+void Point::SetLocal( Vector2 local )
+{
+  mImpl->local = local;
+}
+
+void Point::SetScreen( Vector2 screen )
+{
+  mImpl->screen = screen;
+}
+
+} // namespace Dali
---
+++ dali/public-api/events/touch-event-handle.h
@@ -0,0 +1,143 @@
+#ifndef __DALI_TOUCH_EVENT_HANDLE_H__
+#define __DALI_TOUCH_EVENT_HANDLE_H__
+
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+#include <dali/public-api/common/dali-common.h>
+
+namespace Dali
+{
+/**
+ * @addtogroup dali_core_events
+ * @{
+ */
+
+class Point;
+
+/**
+ * @brief Touch events are a collection of points at a specific moment in time.
+ *
+ * When a multi-touch event occurs, each point represents the points that are currently being
+ * touched or the points where a touch has stopped.
+ * @SINCE_1_1.36
+ */
+struct DALI_IMPORT_API TouchEventHandle
+{
+  // Construction & Destruction
+
+  /**
+   * @brief Default constructor
+   *
+   * @SINCE_1_1.36
+   */
+  TouchEventHandle();
+
+  /**
+   * @brief Constructor
+   *
+   * @SINCE_1_1.36
+   * @param[in]  time  The time the event occurred
+   */
+  TouchEventHandle( unsigned long time );
+
+  /**
+   * @brief Copy constructor
+   *
+   * @SINCE_1_1.36
+   * @param[in]  other  The TouchEventHandle to copy from.
+   */
+  TouchEventHandle( const TouchEventHandle& other );
+
+  /**
+   * @brief Destructor
+   *
+   * @SINCE_1_1.36
+   */
+  ~TouchEventHandle();
+
+  // Operators
+
+  /**
+   * @brief Assignment Operator
+   *
+   * @SINCE_1_1.36
+   * @param[in]  other  The TouchEventHandle to copy from.
+   */
+  TouchEventHandle& operator=( const TouchEventHandle& other );
+
+  // Getters
+
+  /**
+   * @brief Returns the time (in ms) that the touch event occurred.
+   *
+   * @SINCE_1_1.36
+   * @return The time (in ms) that the touch event occurred.
+   */
+  unsigned long GetTime() const;
+
+  /**
+   * @brief Returns the total number of points in this TouchEventHandle.
+   *
+   * @SINCE_1_1.36
+   * @return Total number of Points.
+   */
+  unsigned int GetPointCount() const;
+
+  /**
+   * @brief Returns a point at the index requested.
+   *
+   * The first point in the set is always the primary point (i.e. the first point touched in a multi-touch event).
+   *
+   * @SINCE_1_1.36
+   * @param[in]  point  The index of the required Point.
+   * @return A reference to the Point at the position requested
+   * @note point should be less than the value returned by GetPointCount(). Will assert if out of range.
+   */
+  const Point& GetPoint( unsigned int point ) const;
+
+  // Setters
+
+  /**
+   * @brief Sets the time (in ms) that the touch event occurred.
+   *
+   * @SINCE_1_1.36
+   * @param[in]  time  The time (in ms) that the touch event occurred.
+   */
+  void SetTime( unsigned long time );
+
+  /**
+   * @brief Adds a point to this touch event handler.
+   *
+   * @SINCE_1_1.36
+   * @param[in]  point  The point to add to the touch event handler.
+   */
+  void AddPoint( const Point& point );
+
+private:
+
+  struct Impl;
+  Impl* mImpl;
+};
+
+/**
+ * @}
+ */
+} // namespace Dali
+
+#endif // __DALI_TOUCH_EVENT_HANDLE_H__

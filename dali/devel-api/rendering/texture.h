91af8efb1db697c15f110d943f89c1fe2fdaa9cd
Ferran Sole <ferran.sole@samsung.com>
2016-05-27 09:53:58 +0100

    Added Texture object and FrameBuffer object to rendering API
    * New Texture object which can be used in TextureSets to provide data to shaders
    * Textures can be 2D or Cube maps and can have mipmaps
    * Mipmaps can be generated manually or automatically by the GPU
    * Added sampling modes which requires mipmaps (NEAREST_MIPMAP_NEAREST,LINEAR_MIPMAP_NEAREST,...)
    * New FrameBuffer object which can use Texture as color output
    * Added API to set FrameBuffer as the target for a RenderTask
    
    Change-Id: Ia41e9cd11b3a4b4a49e5141fabf90a9cc50b1a97

---
+++ dali/devel-api/rendering/texture.h
@@ -0,0 +1,145 @@
+#ifndef DALI_TEXTURE_H
+#define DALI_TEXTURE_H
+
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// INTERNAL INCLUDES
+#include <dali/public-api/object/base-handle.h>
+#include <dali/public-api/object/property-index-ranges.h> // DEFAULT_DERIVED_HANDLE_PROPERTY_START_INDEX
+#include <dali/public-api/images/pixel.h>
+
+namespace Dali
+{
+
+namespace Internal DALI_INTERNAL
+{
+class NewTexture;
+}
+
+struct TextureUploadParams
+{
+  unsigned int layer;       ///< Specifies the layer in a cube map texture. 0 is GL_TEXTURE_CUBE_MAP_POSITIVE_X, 1 is GL_TEXTURE_CUBE_MAP_NEGATIVE_X
+                            ///  2 is GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 3 is GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 3 is GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 4 is GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+  unsigned int mipmap;      ///< Specifies the level-of-detail number. Level 0 is the base image level. Level n is the nth mipmap reduction image.
+  unsigned int x0;          ///< Specifies a texel offset in the x direction within the texture array.
+  unsigned int y0;          ///< Specifies a texel offset in the y direction within the texture array.
+  unsigned int width;       ///< Specifies the width of the texture subimage
+  unsigned int height;      ///< Specifies the height of the texture subimage.
+};
+
+namespace TextureType
+{
+  enum Type
+  {
+    TEXTURE_2D,
+    TEXTURE_CUBE
+  };
+}
+
+/**
+ * @brief Texture represents a texture object used as input or output by shaders
+ */
+class DALI_IMPORT_API Texture : public BaseHandle
+{
+public:
+
+  /**
+   * @brief Creates a new Texture object
+   *
+   * @param[in] type The type of the texture
+   * @param[in] format The format of the pixel data
+   * @param[in] width The width of the texture
+   * @param[in] height The height of the texture
+   * @return A handle to a newly allocated Texture
+   */
+  static Texture New( TextureType::Type type, Pixel::Format format, unsigned int width, unsigned int height );
+
+  /**
+   * @brief Default constructor, creates an empty handle
+   */
+  Texture();
+
+  /**
+   * @brief Destructor
+   */
+  ~Texture();
+
+  /**
+   * @brief Copy constructor, creates a new handle to the same object
+   *
+   * @param[in] handle Handle to an object
+   */
+  Texture( const Texture& handle );
+
+  /**
+   * @brief Downcast to a texture.
+   *
+   * If not the returned handle is left uninitialized.
+   * @param[in] handle to an object
+   * @return texture handle or an uninitialized handle
+   */
+  static Texture DownCast( BaseHandle handle );
+
+  /**
+   * @brief Assignment operator, changes this handle to point at the same object
+   *
+   * @param[in] handle Handle to an object
+   * @return Reference to the assigned object
+   */
+  Texture& operator=( const Texture& handle );
+
+  /**
+   * @brief Upload data to the texture
+   * @param[in] buffer A vector with the data to be uploaded
+   * @param[in] params A struct with the parameters for the upload. See TextureUploadParams
+   * @note buffer data will be moved after this call. If the application needs to keep the data,
+   * it will have to pass a copy of the original buffer into this function, otherwise, the data
+   * will be lost
+   */
+  void Upload( Vector<unsigned char>& buffer, const TextureUploadParams& params );
+
+  /**
+   * @brief Generate mipmaps for the texture
+   * This will auto generate all the mipmaps for the texture based on the data in the base level
+   */
+  void GenerateMipmaps();
+
+  /**
+   * @brief Returns de width of the texture
+   * @return The width, in pixels, of the texture
+   */
+  unsigned int GetWidth() const;
+
+  /**
+   * @brief Returns de height of the texture
+   * @return The height, in pixels, of the texture
+   */
+  unsigned int GetHeight() const;
+
+public:
+  /**
+   * @brief The constructor
+   *
+   * @param [in] pointer A pointer to a newly allocated Texture
+   */
+  explicit DALI_INTERNAL Texture( Internal::NewTexture* pointer );
+};
+
+} //namespace Dali
+
+#endif // DALI_TEXTURE_H

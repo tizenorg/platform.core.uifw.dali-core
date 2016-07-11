ad00908650533bbf193630177550b0300f7a5c67
Xiangyin Ma <x1.ma@samsung.com>
2016-07-11 09:57:23 -0700

    Remove Geometry::QUAD() API
    Change-Id: I0cb60bb5b62282017de6221556c5859a3f343634

--- automated-tests/src/dali/dali-test-suite-utils/test-actor-utils.cpp
+++ automated-tests/src/dali/dali-test-suite-utils/test-actor-utils.cpp
@@ -28,6 +28,8 @@ namespace Dali
 
 namespace
 {
+const char * const TEXTURE_UNIFORM_NAME( "sTexture" );
+
 const char* VERTEX_SHADER = DALI_COMPOSE_SHADER(
   attribute mediump vec2 aPosition;\n
   uniform mediump mat4 uMvpMatrix;\n
@@ -65,7 +67,7 @@ Actor CreateRenderableActor( Image texture )
 Actor CreateRenderableActor( Image texture, const std::string& vertexShader, const std::string& fragmentShader )
 {
   // Create the geometry
-  Geometry geometry = Geometry::QUAD();
+  Geometry geometry = CreateQuadGeometry();
 
   // Create Shader
   Shader shader = Shader::New( vertexShader, fragmentShader );

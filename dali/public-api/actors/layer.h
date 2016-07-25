b206dc720c62c70d392a55ddff6beffff6c7471f
Kimmo Hoikka <kimmo.hoikka@samsung.com>
2016-07-25 18:34:21 +0100

    introduce LAYER_UI to replace LAYER_2D, deprecate latter
    Change-Id: I6228bf0e8f3153918fdac8759b0d45e8c41027e2

--- dali/public-api/actors/layer.h
+++ dali/public-api/actors/layer.h
@@ -107,10 +107,17 @@ public:
   enum Behavior
   {
     /**
-     * @brief Layer doesn't make use of the depth test (default mode).
+     * @DEPRECATED_1_1.45, use LAYER_UI instead
+     * @brief UI control rendering mode
+     * @SINCE_1_0.0
+     * @see LAYER_UI
+     */
+    LAYER_2D,
+
+    /**
+     * @brief UI control rendering mode (default mode).
      *
-     * This mode is expected to have better performance than the 3D mode.
-     * When using this mode any ordering would be with respect to tree level of each Actor.
+     * When using this mode renderer ordering will be respect to tree level of each Actor.
      *
      * For the following actor tree of the Layer1 object, D and E hide B, B and C hides A,
      * and F hides C, regardless of their Z positions.
@@ -129,9 +136,11 @@ public:
      *
      * @endcode
      *
-     * @SINCE_1_0.0
+     * This mode is expected to have better performance than LAYER_3D as renderers can be sorted more efficiently.
+     *
+     * @SINCE_1_1.45
      */
-    LAYER_2D,
+    LAYER_UI = LAYER_2D,
 
     /**
      * @brief Layer will use depth test.
@@ -141,15 +150,16 @@ public:
      * those layers regardless of their Z positions (see Layer::Raise() and Layer::Lower()).
      * Opaque renderers are drawn first and write to the depth buffer.
      * Then transparent renderers are drawn with depth test enabled but depth write switched off.
-     * Transparent renderers are drawn based on their distance from the camera (painter's algorithm).
+     * Transparent renderers are drawn based on their distance from the camera.
      * A renderers DEPTH_INDEX property is used to offset the distance to the camera when ordering transparent renderers.
      * This is useful if you want to define the draw order of two or more transparent renderers that are
      * equal distance from the camera.
-     * Unlike LAYER_2D, parent-child relationship does not affect rendering order at all.
+     * Unlike LAYER_UI, parent-child relationship does not affect rendering order at all.
      *
      * @SINCE_1_0.0
      */
-    LAYER_3D,
+    LAYER_3D
+
   };
 
   /**
